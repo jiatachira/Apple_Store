e0b65c36afdef98b44b270031d12789ff6e14754
Achira Dasanayaka <jiat.achira@gmail.com>
2025-09-02 12:46:01 +0530

    run login

--- app/src/main/java/com/example/apple_store/LoginActivity.java
+++ app/src/main/java/com/example/apple_store/LoginActivity.java
@@ -1,14 +1,13 @@
 package com.example.apple_store;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
